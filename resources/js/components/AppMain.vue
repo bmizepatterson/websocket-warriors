00916f2baaea9f43a62f6cd118fdf22a9aefa23f
Ben Patterson <ben.patterson@avnet.com>
2019-05-07 16:27:06 -0400

    Create AppPlayerlist component

--- resources/js/components/AppMain.vue
+++ resources/js/components/AppMain.vue
@@ -3,40 +3,26 @@
         <p class="game-code">Game Code: {{ $root.game.code }}</p>
         <div class="app row no-gutters" style="height: 600px;">
             <app-board />
-
-            <div class="player-list">
-                <ul class="list-group">
-                    <li v-for="user in $root.users" :key="user.name" class="list-group-item border-0">
-                        {{ user.name }}
-                        <span class="float-right">{{ user.score }}</span>
-                    </li>
-                </ul>
-            </div>
+            <app-player-list />
         </div>
     </div>
 </template>
 
 <script>
 import AppBoard from './AppBoard.vue';
+import AppPlayerList from './AppPlayerList.vue';
 
 export default {
-    data() {
-        return {
-        }
-    },
-
     components: {
         AppBoard,
+        AppPlayerList,
     },
 
     mounted() {
-        const self = this;
-        const channel = 'game.' + self.$root.game.id;
-        console.log('Connecting to ' + channel);
-        Echo.channel(channel)
+        Echo.channel('game.' + this.$root.game.id)
             .listen('UserScoreUpdated', (e) => {
                 console.log('Received score update:', e);
-                self.$root.users.filter(u => u.name === e.user)[0].score = e.score;
+                this.$root.users.filter(u => u.name === e.user)[0].score = e.score;
             });
     }
 
@@ -52,24 +38,5 @@ export default {
 }
 .app {
     box-shadow: $shadow-drama;
-    .player-list {
-        width: 300px;
-        background-color: $primary;
-        letter-spacing: 0;
-
-        .list-group-item {
-            background: transparent;
-            border-radius: 0;
-            transition: background-color 0.2s;
-
-            &:hover {
-                background: darken($primary, 10%);
-            }
-        }
-
-        .badge {
-            font-size: inherit;
-        }
-    }
 }
 </style>
---
+++ resources/js/components/AppPlayerList.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="player-list">
+        <ul class="list-group">
+            <li v-for="user in $root.users" :key="user.name" class="list-group-item border-0">
+                {{ user.name }}
+                <span class="float-right">{{ user.score }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.player-list {
+    width: 300px;
+    background-color: $primary;
+    letter-spacing: 0;
+
+    .list-group-item {
+        background: transparent;
+        border-radius: 0;
+        transition: background-color 0.2s;
+
+        &:hover {
+            background: darken($primary, 10%);
+        }
+    }
+
+    .badge {
+        font-size: inherit;
+    }
+}
+</style>
